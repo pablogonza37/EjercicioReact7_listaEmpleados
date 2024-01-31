d06fe52107d1998a531a241b10ab7968cb2b408a
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-31 13:03:33 -0300

    se corrigieron detalles del maquetado

--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -3,7 +3,7 @@ import EmpleadoAvatar from "./EmpleadoAvatar";
 
 const EmpleadoRow = ({ empleado }) => {
   return (
-    <Card className="d-flex flex-row p-sm-2 px-lg-5 bg-light-subtle bg-light">
+    <Card className="d-flex flex-row p-sm-2 px-lg-5 bg-light-subtle bg-light shadow-lg m-1">
       <EmpleadoAvatar empleado={empleado}></EmpleadoAvatar>
       <Card.Body className="w-75 d-flex flex-column text-start justify-content-center ms-lg-5 ms-md-3 ms-2 px-lg-5">
         <Card.Title>{empleado.fullName}</Card.Title>
