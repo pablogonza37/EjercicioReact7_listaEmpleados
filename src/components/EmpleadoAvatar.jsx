9b2fddbfbc3903b76f1c739b6d781c3a987e80f2
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-31 21:19:43 -0300

    correccion de rutas de imagenes

--- src/components/EmpleadoAvatar.jsx
+++ src/components/EmpleadoAvatar.jsx
@@ -1,9 +1,11 @@
-import imagenes from "../asset/imagenes";
-
 const EmpleadoAvatar = ({ empleado }) => {
- 
-
-  return <img variant="top" src={imagenes[empleado.id -1]} className="imgAvatar w-25 img-fluid" />;
+  return (
+    <img
+      variant="top"
+      src={`./src/asset/img/${empleado.pic}`}
+      className="imgAvatar w-25 img-fluid"
+    />
+  );
 };
 
 export default EmpleadoAvatar;
